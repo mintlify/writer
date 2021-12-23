58a403ec1f0ceab85ff6d561c5692e6d27d62492
Han Wang <hyw2@cornell.edu>
2021-12-23 11:06:01 -0500

    Add dist folder

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import axios from 'axios';
 import { getHighlightedText, getInsertPosition, addComments } from './helpers/utils';
 import { changeProgressColor, removeProgressColor } from './helpers/ui';
+import { resolve } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "docs" is now active!');
@@ -11,25 +12,40 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.withProgress({
       location: vscode.ProgressLocation.Notification,
       title: 'Generating documentation',
-    }, () => new Promise(async (resolve, reject) => {
-			changeProgressColor();
-			const editor = vscode.window.activeTextEditor;
-
-			if (editor == null) {
+    }, async () => {
+			const docsPromise = new Promise(async (resolve, reject) => {
+				changeProgressColor();
+				const editor = vscode.window.activeTextEditor;
+	
+				if (editor == null) {
+					removeProgressColor();
+					return resolve('No code selected');
+				}
+	
+				const highlightedText = getHighlightedText(editor);
+				const { data: docstring } = await axios.post('http://localhost:5000/docs/generate/function', { code: highlightedText });
+				const docstringWithComments = addComments(docstring, editor.document.fileName);
+				const snippet = new vscode.SnippetString(`${docstringWithComments}\n`);
+				const insertPosition = getInsertPosition(editor);
+				editor.insertSnippet(snippet, insertPosition);
+				
 				removeProgressColor();
-				return reject('No code selected');
-			}
+				return resolve('Completed');
+			});
 
-			const highlightedText = getHighlightedText(editor);
-			const { data: docstring } = await axios.post('http://localhost:5000/docs/generate/function', { code: highlightedText });
-			const docstringWithComments = addComments(docstring, editor.document.fileName);
-			const snippet = new vscode.SnippetString(`${docstringWithComments}\n`);
-			const insertPosition = getInsertPosition(editor);
-			editor.insertSnippet(snippet, insertPosition);
-			
-			removeProgressColor();
-			resolve('Completed');
-		}));
+			const timeout = new Promise((resolve, _) => {
+				setTimeout(() => {
+					resolve('Timeout');
+				}, 8000);
+			});
+
+			const firstToFinish = await Promise.race([docsPromise, timeout]);
+			console.log(firstToFinish);
+			if (firstToFinish === 'Timeout') {
+				vscode.window.showErrorMessage('Error: Generating documentation timed out');
+			}
+			resolve('Either time out or completed');
+		});
 	});
 
 	context.subscriptions.push(disposable);
