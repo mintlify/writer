b54428be4e28547a3cd01716ff06c5ee387c16ac
Han Wang <hyw2@cornell.edu>
2021-12-23 22:36:53 -0500

    Add error handling for no code selected

--- src/extension.ts
+++ src/extension.ts
@@ -8,44 +8,52 @@ import { DOCS_WRITE } from './helpers/api';
 export function activate(context: vscode.ExtensionContext) {
 	// All active events can be put herex
 	const disposable = vscode.commands.registerCommand('docs.write', async () => {
+		changeProgressColor();
+		const editor = vscode.window.activeTextEditor;
+		if (editor == null) {
+			removeProgressColor();
+			return;
+		}
+
+		const highlightedText = getHighlightedText(editor);
+		if (!highlightedText) {
+			vscode.window.showErrorMessage('No code selected');
+			return;
+		}
+
+		const { languageId } = editor.document;
 
 		vscode.window.withProgress({
       location: vscode.ProgressLocation.Notification,
       title: 'Generating documentation',
     }, async () => {
 			const docsPromise = new Promise(async (resolve, _) => {
-				changeProgressColor();
-				const editor = vscode.window.activeTextEditor;
-	
-				if (editor == null) {
+				try {
+					const { data: docstring } = await axios.post(DOCS_WRITE,
+						{
+							code: highlightedText,
+							languageId,
+							commented: true,
+						});
+					const snippet = new vscode.SnippetString(`${docstring}\n`);
+					const insertPosition = getInsertPosition(editor);
+					editor.insertSnippet(snippet, insertPosition);
+					
 					removeProgressColor();
-					return resolve('No code selected');
+					return resolve('Completed');
+				} catch {
+					vscode.window.showErrorMessage('Error occurred while generating docs');
+					return resolve('Error');
 				}
-	
-				const highlightedText = getHighlightedText(editor);
-				const { languageId } = editor.document;
-				const { data: docstring } = await axios.post(DOCS_WRITE,
-					{
-						code: highlightedText,
-						languageId,
-						commented: true,
-					});
-				const snippet = new vscode.SnippetString(`${docstring}\n`);
-				const insertPosition = getInsertPosition(editor);
-				editor.insertSnippet(snippet, insertPosition);
-				
-				removeProgressColor();
-				return resolve('Completed');
 			});
 
 			const timeout = new Promise((resolve, _) => {
 				setTimeout(() => {
 					resolve('Timeout');
-				}, 8000);
+				}, 15000);
 			});
 
 			const firstToFinish = await Promise.race([docsPromise, timeout]);
-			console.log(firstToFinish);
 			if (firstToFinish === 'Timeout') {
 				vscode.window.showErrorMessage('Error: Generating documentation timed out');
 			}
