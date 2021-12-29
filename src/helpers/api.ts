f1243434ccd796ebaa886278aa8e694d113bcbdc
Han Wang <hyw2@cornell.edu>
2021-12-29 18:59:06 -0500

    Revert making isDev to true

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,4 +1,4 @@
-const isDev = true;
+const isDev = false;
 
 export const MINTBASE = isDev ? 'http://localhost:5000' : 'https://api.mintlify.com';
 export const DOCS_WRITE = MINTBASE + '/docs/write';
