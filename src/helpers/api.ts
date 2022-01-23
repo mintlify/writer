7ad14cbf804dcff0c5e204e717b56d813671cf73
Han Wang <hyw2@cornell.edu>
2022-01-23 00:01:24 -0800

    Use preview endpoint for hover

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,5 +1,6 @@
-const isDev = false;
+const isDev = true;
 
 export const MINTBASE = isDev ? 'http://localhost:5000' : 'https://api.mintlify.com';
 export const DOCS_WRITE = MINTBASE + '/docs/write/v2';
+export const DOCS_PREVIEW = MINTBASE + '/docs/preview';
 export const FEEDBACK = MINTBASE + '/docs/feedback';
