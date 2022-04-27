359db29e7167ef3f08193ace9b84957634ed58f3
Han Wang <hyw2@cornell.edu>
2022-04-27 13:11:03 -0400

    Add server deploy env

--- server/index.ts
+++ server/index.ts
@@ -21,8 +21,7 @@ const dd_options = {
   'response_code':true,
   'tags': ['app:my_app']
 };
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line
 const connect_datadog = require('connect-datadog')(dd_options);
 
 app.use(cors());
