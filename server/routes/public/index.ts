16c334568bdfebab8be4624cf4ab8cf0e8ca287f
Hahnbee Lee <hl985@cornell.edu>
2022-05-04 00:00:37 -0400

    uncomment public api

--- server/routes/public/index.ts
+++ server/routes/public/index.ts
@@ -35,30 +35,29 @@ const apiLimiter = rateLimit({
 
 // https://docs.mintlify.com/reference/documentation
 publicRouter.post('/document', apiLimiter, apiMiddleware, async (req, res) => {
-  return res.status(400).send({error: 'The Mintlify API is currently being updated. Please email hi@mintlify for urgent authorization'});
-  // const { hashedKey } = res.locals;
-  // const { code, commented, language, format, context } = req.body;
+  const { hashedKey } = res.locals;
+  const { code, commented, language, format, context } = req.body;
 
-  // if (!code) {
-  //   return res.status(400).send({error: 'No code provided'});
-  // }
+  if (!code) {
+    return res.status(400).send({error: 'No code provided'});
+  }
 
-  // const body = {
-  //   code,
-  //   languageId: language,
-  //   commented: commented || false,
-  //   userId: hashedKey,
-  //   docStyle: format || 'Auto-detect',
-  //   context: context || code,
-  //   source: 'api',
-  // }
+  const body = {
+    code,
+    languageId: language,
+    commented: commented || false,
+    userId: hashedKey,
+    docStyle: format || 'Auto-detect',
+    context: context || code,
+    source: 'api',
+  }
 
-  // try {
-  //   const { docstring } = await getDocument(body);
-  //   return res.status(200).send({documentation: docstring});
-  // } catch {
-  //   return res.status(400).send({error: 'Error generating documentation from code'});
-  // }
+  try {
+    const { docstring } = await getDocument(body);
+    return res.status(200).send({documentation: docstring});
+  } catch {
+    return res.status(400).send({error: 'Error generating documentation from code'});
+  }
 });
 
 publicRouter.get('/list/languages', apiLimiter, apiMiddleware, async (_, res) => {
