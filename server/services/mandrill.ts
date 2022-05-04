9a01dff8b70e024c6901f50a9511399c249f31c9
Hahnbee Lee <hl985@cornell.edu>
2022-05-04 00:00:50 -0400

    update mandrill email from

--- server/services/mandrill.ts
+++ server/services/mandrill.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const TEMPLATE_SLUG = 'ai-doc-writer-invite-to-team';
 
-export const sendInviteEmail = async (toEmail: string, fromEmail = 'hi@mintlify.com') => {
+export const sendInviteEmail = async (toEmail: string, fromEmail = 'founders@mintlify.com') => {
   await axios.post('https://mandrillapp.com/api/1.0/messages/send-template', {
     key: process.env.MAILCHIMP_TRANSACTIONAL_KEY,
     template_name: TEMPLATE_SLUG,
