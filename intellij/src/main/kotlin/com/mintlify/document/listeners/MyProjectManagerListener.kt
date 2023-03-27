c34639541e172e9a0a558852a32c29ac0da6b52b
shizeying <w741069229@163.com>
2023-03-27 21:12:00 +0800

    feat: update ProjectManagerListener to StartupActivity

--- intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
+++ intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
@@ -2,12 +2,13 @@ package com.mintlify.document.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.StartupActivity
 import com.mintlify.document.services.MyProjectService
 
-internal class MyProjectManagerListener : ProjectManagerListener {
-
-    override fun projectOpened(project: Project) {
+internal class MyProjectManagerListener : StartupActivity {
+    override fun runActivity(project: Project) {
         project.service<MyProjectService>()
     }
+
+
 }
