b673242b4af9529b685de87af00c82e0aa6350f8
shizyeing <w741069229@163.com>
2023-03-28 13:04:11 +0800

    feature: support IDEA 223* version (IDEA 2022.3)

--- intellij/build.gradle.kts
+++ intellij/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20-RC2"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.5.2"
+    id("org.jetbrains.intellij") version "1.13.2"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
 }
